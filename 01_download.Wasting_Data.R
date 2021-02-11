b6d4dc4b753c35480d46c25dd2cab9bdbb66bed4
ganyy825 <ganyy825@bu.edu>
2021-02-10 21:38:01 -0500

    added some code

--- 01_download.Wasting_Data.R
+++ 01_download.Wasting_Data.R
@@ -7,3 +7,7 @@ data <- read_csv(
 git config --global user.email "haydendu@bu.edu"
 git config --global user.name "haydendu"
 
+{r, eval=FALSE}
+library(ggplot2)
+boxplot(data$`Average WD (%)`~Transect,data=data, main="not sure what to put here",
+        xlab="Transect", ylab="Average wasting disease %")
